50d2277a33881cd00b86ae527db8d8b0cf37d3c5
Brajeshwar Oinam <brajeshwar@oinam.com>
2014-01-10 19:39:24 +0530

    Footer text edit.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
       |
       <a href="<?php bloginfo('rss2_url'); ?>" title="Feed for posts">Entries (RSS)</a>
       |
-      Get Nishita Pro Version and other Themes at <a href="http://theme6.com/" title="Theme6">Theme6</a>.
+      Get Nishita (Pro) and more Themes at <a href="http://theme6.com/" title="Theme6">Theme6</a>.
     </p>
   </div>
 
