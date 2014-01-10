71639f31fe3e045860c753d5a51f8892c3abb0ae
Brajeshwar Oinam <brajeshwar@oinam.com>
2014-01-10 19:53:44 +0530

    Footer updated with source info.

--- footer.php
+++ footer.php
@@ -4,7 +4,8 @@
       |
       <a href="<?php bloginfo('rss2_url'); ?>" title="Feed for posts">Entries (RSS)</a>
       |
-      Get Nishita (Pro) and more Themes at <a href="http://theme6.com/" title="Theme6">Theme6</a>.
+      Get <a href="http://theme6.com/nishita/" title="Nishita Photography Theme">Nishita (Pro)</a>
+      and more Themes at <a href="http://theme6.com/" title="Theme6">Theme6</a>.
     </p>
   </div>
 
